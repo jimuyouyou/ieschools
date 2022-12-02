d86fcd25a04b743f5889a6b6a2bcfb595f1814b7
jimuyouyou <qinfuxian2014@outlook.com>
2022-12-02 14:48:21 +0800

    schools data

---
+++ data/preprocess.js
@@ -0,0 +1,21 @@
+// quick start: cd data ; node preprocess.js
+
+const XLSX = require('xlsx');
+const path = require('path');
+const fs = require('fs');
+const { preProcessFile } = require('typescript');
+
+// School
+const wb = XLSX.readFile(path.resolve('./ie_school_2022.xlsx'));
+const schools = XLSX.utils.sheet_to_json(wb.Sheets[wb.SheetNames[2]]);
+schools.pop()
+fs.writeFileSync(path.resolve('./schools.json'), JSON.stringify(schools));
+
+// County
+counties = new Set()
+for (i = 0; i < schools.length; i++) {
+  counties.add(schools[i]['County Description'])
+}
+countLists = Array.from(counties).map(c => {return {name: c}});
+fs.writeFileSync(path.resolve('./counties.json'), JSON.stringify(countLists));
+
