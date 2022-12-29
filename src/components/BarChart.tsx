0e9a3f1fe29b9c978850ba0023ee4efa40713076
jimuyouyou <qinfuxian2014@outlook.com>
2022-12-29 15:36:30 +0800

    bar chart

--- src/components/BarChart.tsx
+++ src/components/BarChart.tsx
@@ -17,13 +17,50 @@ const propTypes = {
 };
 
 type BarChartProps = PropTypes.InferProps<typeof propTypes>;
-
+const colors = [
+  '#309975',
+  '#58b368',
+  '#dad873',
+  '#efeeb4',
+  '#454d66',
+  '#e74645',
+  '#fb7756',
+  '#facd60',
+  '#fdfa66',
+  '#1ac0c6',
+  '#22577a',
+  '#38a3a5',
+  '#57cc99',
+  '#80ed99',
+  '#c7f9cc',
+];
 export function BarChart(props: BarChartProps) {
   const { headers, rows, onChange } = props;
-  console.log('Barchart', rows)
+  const [rowInd, setRowInd] = useState(0)
+
+  const row = rows ? rows[rowInd] : []
+  const cells = row.filter((r: CellType) => typeof r.desc !== 'string' && !['total', 'd'].includes(r.id));
+  console.log('cells', cells);
+  const totalCell = row.find((r: CellType) => 'total' === r.id);
+  const total: number = totalCell ? totalCell.desc : 100;
+
   return (
     <div className='bar-chart-wrapper'>
-      bar-chart
+      {cells && cells.map((cell: CellType, cellIndex: number) => {
+        const val: number = +cell.desc;
+        const percent = Math.round(val * 100 / total);
+        return (
+          <div key={cellIndex} className='bar-wrapper'>
+            <div className='bar-bar' style={{ width: `${percent}%`, background: colors[cellIndex] }}>
+              {cell.desc}
+            </div>
+            <div className='bar-label'>
+              {cell.id}
+            </div>
+          </div>
+        )
+      })}
+
     </div>
   );
 }
